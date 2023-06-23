c810d3297069ebc833396dd0ec84586cb2724ec6
Santiago Mamani <santiagojmamani@gmail.com>
2023-06-23 12:16:26 -0300

    Update l2_snack_bar.dart

--- lib/widgets/l2_snack_bar.dart
+++ lib/widgets/l2_snack_bar.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 
-class L2SnackBar extends StatelessWidget {
-  const L2SnackBar({Key? key, required this.text}) : super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return SnackBar(
-      behavior: SnackBarBehavior.floating,
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-      content: Text(text),
-    );
-  }
+// ignore: non_constant_identifier_names
+SnackBar L2SnackBar(String text) {
+  return SnackBar(
+    behavior: SnackBarBehavior.floating,
+    margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+    content: Text(text),
+  );
 }
